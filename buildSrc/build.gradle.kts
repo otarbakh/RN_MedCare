1d3bc912ab67f28e53ab9cf85b6b1a57700d2976
otarbakh <otarbakh@gmail.com>
2023-12-03 22:20:52 +0400

    added for demonstration dagger hilt in app.

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins{
+    `kotlin-dsl`
+}
+repositories {
+    google()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    implementation("com.android.tools.build:gradle:8.1.1")
+}
